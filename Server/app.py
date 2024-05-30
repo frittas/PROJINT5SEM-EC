04107775aaecd0b8168383aaf647991ea631d719
frittas <faelfreitas00@gmail.com>
2024-05-30 14:54:41 -0300

    facereco tunnings

--- Server/app.py
+++ Server/app.py
@@ -4,6 +4,10 @@ import cv2
 from modules.FaceRecognition import FaceRecognition
 from server.modules.ImageRepository import ImageRepository
 
+
+
+from multiprocessing.pool import ThreadPool
+
 app = Flask(__name__)
 
 
@@ -14,37 +18,36 @@ facereco = FaceRecognition()
 
 repository = ImageRepository(connection_string, db_name, collection_name)
 
-
-# camera_port = 0
-# camera = cv2.VideoCapture(camera_port, cv2.CAP_DSHOW)  # this makes a web cam object
-
-# retval, frame = camera.read()
-
-# imgencode = cv2.imencode(".jpg", frame)[1].tostring()
-
-# repository.salvar_imagem(imgencode, "rafael")
-
+camera_port = 0
+camera = cv2.VideoCapture(camera_port)  # this makes a web cam object
 
 def generate_frames():
-
-    camera_port = 0
-    camera = cv2.VideoCapture(camera_port, cv2.CAP_DSHOW)  # this makes a web cam object
-
+   
     while True:
         retval, frame = camera.read()
+        if not retval:
+            break
 
         reco = facereco.compararFaces(frame)
-        print(f"Rosto detectado: {reco}")
+        if reco:
+            print(f"Rosto detectado: {reco}")
 
         imgencode = cv2.imencode(".jpg", frame)[1]
-        stringData = imgencode.tostring()
+        stringData = imgencode.tobytes()
         yield (
             b"--frame\r\n" b"Content-Type: text/plain\r\n\r\n" + stringData + b"\r\n"
         )
 
-    del camera
+
+pool = ThreadPool(processes=1)
+async_result = pool.apply_async(generate_frames)
 
 
+def save_face():
+    retval, frame = camera.read()
+    imgencode = cv2.imencode(".jpg", frame)[1].tobytes()
+    repository.salvar_imagem(imgencode, "Rafael")
+    facereco.carregarImagens()
 
 
 @app.route("/")
@@ -52,12 +55,17 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/save")
+def save():
+    return Response(save_face())
+
+
 @app.route("/video")
 def video():
     return Response(
-        generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
+        async_result.get(), mimetype="multipart/x-mixed-replace; boundary=frame"
     )
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True, threaded=True)
+    app.run(host="localhost", port=8000, debug=True, threaded=True)
--- Server/modules/FaceRecognition.py
+++ Server/modules/FaceRecognition.py
@@ -1,6 +1,7 @@
 import face_recognition
 import os
-import cv2
+import numpy as np
+import cv2 as cv
 from server.modules.ImageRepository import ImageRepository
 
 connection_string = "mongodb://localhost:27017/"  # configuracao do mongo
@@ -10,12 +11,12 @@ collection_name = "imagens"
 
 class FaceRecognition:
     def __init__(self):
-        self.pasta_base = "Server/database"
+        self.pasta_base = "server/database"
         self.nomes_base = []
         self.imagens_base = []
         self.repository = ImageRepository(connection_string, db_name, collection_name)
-        # self.carregarImagens()
-        self.carregarDataBase()
+        self.carregarImagens()
+        # self.carregarDataBase()
 
     def carregarDataBase(self):
         for nome_arquivo in os.listdir(self.pasta_base):
@@ -30,51 +31,60 @@ class FaceRecognition:
             # Verificar se pelo menos uma face foi detectada
             if codificacoes_base:
                 self.imagens_base.extend(codificacoes_base)
+                self.nomes_base.append(nome)
 
     def carregarImagens(self):
         documents = self.repository.bucar_todas_imagens()
+        for doc in documents:
+            image = cv.imdecode(
+                np.frombuffer(doc["imagem"], dtype=np.uint8), cv.IMREAD_COLOR
+            )
+            face_encoding = face_recognition.face_encodings(image)
+            self.imagens_base.extend(face_encoding)
+            self.nomes_base.append(doc["nome"])
 
     def compararFaces(self, frame) -> str:
         # Encontrar faces na imagem da câmera
+        frame_encoding = face_recognition.face_encodings(frame)
         face_locations = face_recognition.face_locations(frame)
-        face_codificacoes = face_recognition.face_encodings(frame, face_locations)
 
         # Comparar cada face encontrada com a lista de imagens da base
-        for face_codificacao in face_codificacoes:
-            correspondencia_encontrada = False
-            for codificacao_base, nome_base in zip(self.imagens_base, self.nomes_base):
-                comparacao = face_recognition.compare_faces(
-                    [codificacao_base], face_codificacao
-                )
+        if frame_encoding:
+            results = face_recognition.compare_faces(
+                self.imagens_base, frame_encoding[0]
+            )
 
-                for top, right, bottom, left in face_locations:
-                    image = cv2.rectangle(
-                        frame, (left, top), (right, bottom), (0, 255, 0), 2
-                    )
-
-                if comparacao[0]:  # Se for uma correspondência
-                    cv2.putText(
+            for result, nome in zip(results, self.nomes_base):
+                if result.T:
+                    for top, right, bottom, left in face_locations:
+                        image = cv.rectangle(
+                            frame, (left, top), (right, bottom), (36, 255, 12), 2
+                        )
+                    cv.putText(
                         image,
-                        nome_base,
-                        (top, int(left/2)),
-                        cv2.FONT_HERSHEY_SIMPLEX,
+                        nome,
+                        (left, top - 20),
+                        cv.FONT_HERSHEY_SIMPLEX,
                         0.9,
                         (36, 255, 12),
                         2,
                     )
-                    correspondencia_encontrada = True
-                    return nome_base
-
-            if not correspondencia_encontrada:
-                cv2.putText(
-                    image,
-                    "INTRUSO SAI DAQUI",
-                    (top, int(left/2)),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.9,
-                    (0, 0, 255),
-                    2,
-                )
+                    return nome
+                else:
+                    for top, right, bottom, left in face_locations:
+                        image = cv.rectangle(
+                            frame, (left, top), (right, bottom), (0, 0, 255), 2
+                        )
+                    cv.putText(
+                        image,
+                        "NAO IDENTIFICADO",
+                        (left - 50, top - 20),
+                        cv.FONT_HERSHEY_SIMPLEX,
+                        0.9,
+                        (0, 0, 255),
+                        2,
+                    )
+                    return "NAO IDENTIFICADO"
 
     def gravarFace(frame):
         return True
